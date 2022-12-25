433beb31fa50128a7e92e57a7fef86789454f3f5
ayeryn <erynli@utexas.edu>
2022-12-24 22:16:11 -0600

    Define instance methods; update .gitignore

--- Report.py
+++ Report.py
@@ -1,2 +1,17 @@
+import calendar
+import os
+
 class Report:
-    pass
+
+    def __init__(self, month, year):
+        self.month = month
+        self.year = year
+        self.cal = calendar.month(int(self.year), int(self.month))
+        self.name = os.getcwd() + '/' + self.year + self.month + '.csv'
+        self.title = 'Monthly Report {}/{}'.format(self.year, self.month)
+    
+    def print_attrs(self):
+        msg = 'month = {}\nyear = {}\n'.format(self.month, self.year)
+        msg += 'cal = {}\nname = {}\n'.format(self.cal, self.name)
+        print(msg)
+        
