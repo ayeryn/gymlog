3bcb25a0c5896b01df6b9bf6a0e83fbf9168039a
ayeryn <erynli@utexas.edu>
2022-12-24 23:11:55 -0600

    Restore user input for month

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from Report import Report
 
 
 def main():
-    # q = 'What month would you like to generated the report? (yyyymm)\n'
-    # request = input(q)
-    # while not (request.isnumeric() and len(request) == 6) :
-    #     request = input(q)
-        
-    # year = request[:4]
-    # month = request[4:]
+    q = 'What month would you like to generated the report? (yyyymm)\n'
+    request = input(q)
+    while not (request.isnumeric() and len(request) == 6):
+        request = input(q)
 
-    year = '2022'
-    month = '12'
+    year = request[:4]
+    month = request[4:]
+
+    # year = '2022'
+    # month = '12'
 
     report = Report(month, year)
     report.process_jnl()
     report.generate_report()
     report.print_report()
+
 
 main()
