cc79fd8120fa34ad4a10234a8275b72f33887e65
ayeryn <erynli@utexas.edu>
2022-12-27 00:22:40 -0600

    closing #9

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from Report import MonthlyReport
+import re
+
+
+def get_input():
+    """ Gather year and month from user
+
+    When ALL mode is selected return 0 for month
+    """
+    year, month = 0, 0
+    q = 'What year would you like to generated the report? (yyyy)\n'
+
+    year = input(q)
+    while not (re.fullmatch('\d{4}$', year)):
+        year = input(q)
+
+    q = 'What month? a for ALL months or mm\n'
+    month = input(q)
+    while not ((re.fullmatch('\d{2}$', month)) or month == 'a'):
+        month = input(q)
+
+    if month == 'a':
+        month = 0
+
+    return year, month
 
 
 def main():
-    # q = 'What month would you like to generated the report? (yyyymm)\n'
-    # request = input(q)
-    # while not (request.isnumeric() and len(request) == 6):
-    #     request = input(q)
 
-    # year = request[:4]
-    # month = request[4:]
+    year, month = get_input()
 
-    year = '2022'
-    month = '12'
+    # year, month = '2022', '12'
 
     report = MonthlyReport(month, year)
     report.print_report()
