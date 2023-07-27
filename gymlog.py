ab1b649b145f93566e20815f04d92c945e9e62de
ayeryn <erynli@utexas.edu>
2023-07-27 18:36:56 -0500

    Add user forms

--- gymlog.py
+++ gymlog.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+# Python module secrets
+app.config["SECRET_KEY"] = "85d7836bac21fafd3ed57a1d18c06518"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
 
 activities = [
     {
@@ -33,5 +41,26 @@ def about():
     return render_template("about.html", title='About')
 
 
-if __name__ == '__main__':
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'test@a.com' and form.password.data == "12345":
+            flash(f"You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash(f"Login unsuccessful! Please check username and password", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
