e59ab1f8b39fe283d354255e58f5d1f32f480a88
ayeryn <erynli@utexas.edu>
2023-08-02 13:31:47 -0500

    Add data models

--- gymlog.py
+++ gymlog.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
@@ -9,6 +10,43 @@ app.config["SECRET_KEY"] = "85d7836bac21fafd3ed57a1d18c06518"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    avatar_file = db.Column(
+        db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    # attendances = db.Relationship('Attendance', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.avatar_file}')"
+
+
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), unique=True, nullable=False)
+    category = db.Column(db.String(15), unique=True, nullable=False)
+    # attendances = db.Relationship('Attendance', backref='activity', lazy=True)
+
+    def __repr__(self):
+        return f"Activity('{self.name}', '{self.category}')"
+
+
+class Attendance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # activity_id = db.Column(db.Integer, db.ForeignKey(
+    #     'activity.id'), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey(
+    #     'user.id'), nullable=False)
+    date_attended = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Attendance('{self.activity_name}', '{self.date_attended}')"
+
 
 activities = [
     {
