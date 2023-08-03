20d9d48f926d46e12ffea61d790ea6d948573062
ayeryn <erynli@utexas.edu>
2023-08-03 12:22:15 -0500

    Fix db path and re-add flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,6 @@ app.config.from_object(Config)
 
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db, render_as_batch=True)
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
 
     SECRET_KEY = os.environ.get(
-        'SECRET_KEY') or "85d7836bac21fafd3ed57a1d18c06518"
+        'SECRET_KEY') or '85d7836bac21fafd3ed57a1d18c06518'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'SQLALCHEMY_DATABASE_URI') or "sqlite:///site.db"
+        'DATABASE_URL') \
+        or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
