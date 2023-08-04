4bd54603643632a4d0bc04a1e37c2654c9d4e2b4
ayeryn <erynli@utexas.edu>
2023-08-03 20:12:59 -0500

    Update account route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db
 from app.models import User, Activity, Attendance
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import current_user, login_user, logout_user, login_required
 
 
@@ -56,7 +56,23 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+
+        flash('You account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.avatar_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
