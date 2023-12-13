8bb82e138504136176cf0bb75b4c3634e4b992f3
ayeryn <erynli@utexas.edu>
2023-12-13 15:45:05 -0600

    add User model

--- app/models.py
+++ app/models.py
@@ -33,4 +33,13 @@ class Attendance(db.Model):
         return self.date_attended < other.date_attended
 
 
-# TODO: Add user
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    avatar_file = db.Column(
+        db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f'User({self.username}, {self.email}, {self.avatar_file})'
