4ea2bb55edc943a2358bf11cce3f0eb45ecd8dbb
ayeryn <erynli@utexas.edu>
2023-12-13 22:43:34 -0600

    start using Black formatter and double quotes

--- app/routes.py
+++ app/routes.py
@@ -13,18 +13,17 @@ from collections import defaultdict
 
 
 def get_quote():
-    r = requests.get(
-        "https://api.quotable.io/quotes/random?tags=motivational|failure")
+    r = requests.get("https://api.quotable.io/quotes/random?tags=motivational|failure")
     data = json.loads(r.content)
     quote_dict = data[0]
-    return quote_dict['content'], quote_dict['author']
+    return quote_dict["content"], quote_dict["author"]
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     quote, author = get_quote()
-    return render_template('home.html', quote=quote, author=author)
+    return render_template("home.html", quote=quote, author=author)
 
 
 """
@@ -32,40 +31,41 @@ Class related endpoints
 """
 
 
-@app.route('/classes')
+@app.route("/classes")
 def classes():
-    page = request.args.get('page', 1, type=int)
-    gym_classes = GymClass.query.order_by(
-        GymClass.name).paginate(page=page, per_page=9)
-    return render_template('classes.html', title='Classes', gym_classes=gym_classes)
+    page = request.args.get("page", 1, type=int)
+    gym_classes = GymClass.query.order_by(GymClass.name).paginate(page=page, per_page=9)
+    return render_template("classes.html", title="Classes", gym_classes=gym_classes)
 
 
 def capitalize_str(s):
     return " ".join([x.capitalize() for x in s.split()])
 
 
-@app.route('/new_class', methods=['GET', 'POST'])
+@app.route("/new_class", methods=["GET", "POST"])
 def new_class():
     form = ClassForm()
 
     if form.validate_on_submit():
-        c = GymClass(name=capitalize_str(form.name.data),
-                     class_type=capitalize_str(form.class_type.data))
+        c = GymClass(
+            name=capitalize_str(form.name.data),
+            class_type=capitalize_str(form.class_type.data),
+        )
         db.session.add(c)
         db.session.commit()
-        flash('New class added!', 'success')
-        return redirect(url_for('classes', title='Classes'))
+        flash("New class added!", "success")
+        return redirect(url_for("classes", title="Classes"))
 
-    return render_template('add_class.html', legend='New Class', form=form)
+    return render_template("add_class.html", legend="New Class", form=form)
 
 
-@app.route('/class/<int:class_id>')
+@app.route("/class/<int:class_id>")
 def show_class(class_id):
     c = GymClass.query.get(class_id)
-    return render_template('class.html', legend=c.name, gymclass=c)
+    return render_template("class.html", legend=c.name, gymclass=c)
 
 
-@app.route('/class/<int:class_id>/update', methods=['GET', 'POST'])
+@app.route("/class/<int:class_id>/update", methods=["GET", "POST"])
 def edit_class(class_id):
     c = GymClass.query.get(class_id)
     form = ClassForm()
@@ -75,26 +75,26 @@ def edit_class(class_id):
         c.class_type = form.class_type.data
         db.session.add(c)
         db.session.commit()
-        flash('Class updated!', 'success')
+        flash("Class updated!", "success")
 
-    elif request.method == 'GET':
+    elif request.method == "GET":
         form.name.data = c.name
         form.class_type.data = c.class_type
-        return render_template('add_class.html', legend='Update class', form=form)
+        return render_template("add_class.html", legend="Update class", form=form)
 
-    return redirect(url_for('classes'))
+    return redirect(url_for("classes"))
 
 
-@app.route('/class/<int:class_id>/delete', methods=['POST'])
+@app.route("/class/<int:class_id>/delete", methods=["POST"])
 def delete_class(class_id):
     c = GymClass.query.get(class_id)
     class_name = c.name
 
     db.session.delete(c)
     db.session.commit()
-    flash(f'{class_name} has been deleted!', 'success')
+    flash(f"{class_name} has been deleted!", "success")
 
-    return redirect(url_for('classes'))
+    return redirect(url_for("classes"))
 
 
 """
@@ -102,32 +102,35 @@ Attendance related endpoints
 """
 
 
-@app.route('/attendances')
+@app.route("/attendances")
 def attendances():
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get("page", 1, type=int)
     attendance_list = Attendance.query.order_by(
-        Attendance.date_attended.desc()).paginate(page=page, per_page=15)
-    return render_template('attendances.html', title='Attendances', attendance_list=attendance_list)
+        Attendance.date_attended.desc()
+    ).paginate(page=page, per_page=15)
+    return render_template(
+        "attendances.html", title="Attendances", attendance_list=attendance_list
+    )
 
 
-@app.route('/new_attendance', methods=['GET', 'POST'])
+@app.route("/new_attendance", methods=["GET", "POST"])
 def new_attendance():
     form = AttendanceForm()
-    form.class_id.choices = [(c.id, c.name)
-                             for c in GymClass.query.all()]
+    form.class_id.choices = [(c.id, c.name) for c in GymClass.query.all()]
 
     if form.validate_on_submit():
-        a = Attendance(class_id=form.class_id.data,
-                       date_attended=form.date_attended.data)
+        a = Attendance(
+            class_id=form.class_id.data, date_attended=form.date_attended.data
+        )
         db.session.add(a)
         db.session.commit()
-        flash(f'New attendance added for {a.class_taken.name}!', 'success')
-        return redirect(url_for('show_class', class_id=a.class_id))
+        flash(f"New attendance added for {a.class_taken.name}!", "success")
+        return redirect(url_for("show_class", class_id=a.class_id))
 
-    return render_template('add_attendance.html', legend='New Attendance', form=form)
+    return render_template("add_attendance.html", legend="New Attendance", form=form)
 
 
-@app.route('/attendance/<int:attendance_id>/update', methods=['GET', 'POST'])
+@app.route("/attendance/<int:attendance_id>/update", methods=["GET", "POST"])
 def edit_attendance(attendance_id):
     a = Attendance.query.get(attendance_id)
     form = AttendanceForm()
@@ -137,29 +140,31 @@ def edit_attendance(attendance_id):
         a.date_attended = form.date_attended.data
         db.session.add(a)
         db.session.commit()
-        flash('Attendance updated!', 'success')
-    elif request.method == 'GET':
+        flash("Attendance updated!", "success")
+    elif request.method == "GET":
         form.class_id.data = a.class_taken.name
         form.date_attended.data = a.date_attended
-        return render_template('add_attendance.html', legend='Update Attendance', form=form)
+        return render_template(
+            "add_attendance.html", legend="Update Attendance", form=form
+        )
 
-    return redirect(url_for('attendances'))
+    return redirect(url_for("attendances"))
 
 
-@app.route('/attendance/<int:attendance_id>/delete', methods=['POST'])
+@app.route("/attendance/<int:attendance_id>/delete", methods=["POST"])
 def delete_attendance(attendance_id):
     a = Attendance.query.get(attendance_id)
     db.session.delete(a)
     db.session.commit()
-    flash(f'Attendance has been deleted!', 'success')
+    flash(f"Attendance has been deleted!", "success")
 
-    return redirect(url_for('attendances'))
+    return redirect(url_for("attendances"))
 
 
 """
 CSV loader
 """
-ALLOWED_EXTENTIONS = set(['.csv'])
+ALLOWED_EXTENTIONS = set([".csv"])
 
 
 def process_csv_row(data, filename):
@@ -175,15 +180,15 @@ def process_csv_row(data, filename):
     return class_name, date_attended
 
 
-@ app.route('/upload', methods=['GET', 'POST'])
+@app.route("/upload", methods=["GET", "POST"])
 def upload_csv():
-    if request.method == 'POST':
-        file = request.files['file']
+    if request.method == "POST":
+        file = request.files["file"]
         f_name, f_ext = os.path.splitext(file.filename)
 
         if file and f_ext in ALLOWED_EXTENTIONS:
-            file = TextIOWrapper(file, encoding='utf-8')
-            csv_reader = csv.reader(file, delimiter=',')
+            file = TextIOWrapper(file, encoding="utf-8")
+            csv_reader = csv.reader(file, delimiter=",")
             for row in csv_reader:
                 class_name, date = process_csv_row(row, f_name)
 
@@ -193,16 +198,16 @@ def upload_csv():
                     c = GymClass(name=class_name)
                     db.session.add(c)
                     db.session.commit()
-                    flash(f'Class {class_name} created from csv', 'success')
+                    flash(f"Class {class_name} created from csv", "success")
 
                 a = Attendance(class_id=c.id, date_attended=date)
                 db.session.add(a)
                 db.session.commit()
-                flash(f'New attendance create for {class_name}', 'success')
+                flash(f"New attendance create for {class_name}", "success")
 
-        return redirect(url_for('classes'))
+        return redirect(url_for("classes"))
 
-    return render_template('upload.html', title="Upload CSV")
+    return render_template("upload.html", title="Upload CSV")
 
 
 """
@@ -213,9 +218,9 @@ Reporting
 @app.route("/monthly_report")
 def monthly_report():
     today = date.today()
-    attendances = Attendance.query \
-        .filter(extract('year', Attendance.date_attended) == today.year) \
-        .filter(extract('month', Attendance.date_attended) == today.month)
+    attendances = Attendance.query.filter(
+        extract("year", Attendance.date_attended) == today.year
+    ).filter(extract("month", Attendance.date_attended) == today.month)
 
     data = defaultdict(int)
     for attendance in attendances:
@@ -224,13 +229,14 @@ def monthly_report():
     labels = [k for k in data.keys()]
     values = [v for v in data.values()]
 
-    return render_template('monthly_report.html', labels=labels, values=values)
+    return render_template("monthly_report.html", labels=labels, values=values)
 
 
 @app.route("/yearly_report")
 def yearly_report():
     attendances = Attendance.query.filter(
-        extract('year', Attendance.date_attended) == date.today().year)
+        extract("year", Attendance.date_attended) == date.today().year
+    )
 
     data = defaultdict(int)
     for attendance in attendances:
@@ -239,7 +245,7 @@ def yearly_report():
     labels = [k for k in data.keys()]
     values = [v for v in data.values()]
 
-    return render_template('yearly_report.html', labels=labels, values=values)
+    return render_template("yearly_report.html", labels=labels, values=values)
 
 
 """
@@ -255,10 +261,12 @@ def register():
     if form.validate_on_submit():
         flash(f"Account created for {form.username.data}!", "success")
         return redirect(url_for("home"))
-    return render_template('register.html', title='Register', form=form)
+    return render_template("register.html", title="Register", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Login', form=form)
+    if form.validate_on_submit:
+        return redirect(url_for("home"))
+    return render_template("login.html", title="Login", form=form)
