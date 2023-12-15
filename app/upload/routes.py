48c4dc3ff6e2788537bc97acaead13728aa1af72
ayeryn <erynli@utexas.edu>
2023-12-15 12:20:25 -0600

    upload: remove utils and update routes. add test csv

--- app/upload/routes.py
+++ app/upload/routes.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template, flash, redirect, url_for, request
 from io import TextIOWrapper
 import os
 import csv
+from datetime import date
 from app import db
 from app.models import GymClass, Attendance
-from app.upload.utils import process_csv_row
+from app.classes.utils import capitalize_str
 
 
 upload = Blueprint("upload", __name__)
@@ -16,13 +17,14 @@ ALLOWED_EXTENSIONS = set([".csv"])
 def upload_csv():
     if request.method == "POST":
         file = request.files["file"]
-        f_name, f_ext = os.path.splitext(file.filename)
+        _, f_ext = os.path.splitext(file.filename)
 
         if file and f_ext in ALLOWED_EXTENSIONS:
             file = TextIOWrapper(file, encoding="utf-8")
             csv_reader = csv.reader(file, delimiter=",")
             for row in csv_reader:
-                class_name, date = process_csv_row(row, f_name)
+                class_name = capitalize_str(row[0])
+                date_attended = date.fromisoformat(row[1])
 
                 # Add GymClass if class doesn't exists
                 c = GymClass.query.filter_by(name=class_name).first()
@@ -32,7 +34,7 @@ def upload_csv():
                     db.session.commit()
                     flash(f"Class {class_name} created from csv", "success")
 
-                a = Attendance(class_id=c.id, date_attended=date)
+                a = Attendance(class_id=c.id, date_attended=date_attended)
                 db.session.add(a)
                 db.session.commit()
                 flash(f"New attendance create for {class_name}", "success")
