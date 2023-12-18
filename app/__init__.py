7d8ff2975fc39043d6ad3c6885d118c6dfbc21a8
ayeryn <erynli@utexas.edu>
2023-12-18 17:29:57 -0600

    Diable flask_migrate for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
+# from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -15,7 +16,7 @@ app.config.from_object(Config)
 
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db, compare_type=True)
+# migrate = Migrate(app, db, compare_type=True)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "users.login"
