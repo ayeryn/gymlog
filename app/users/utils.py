454bf8fd9dd8f15b7de41e00f7a1ca3ed19b2227
ayeryn <erynli@utexas.edu>
2023-12-18 17:46:08 -0600

    move email functions to utils

--- app/users/utils.py
+++ app/users/utils.py
@@ -1,7 +1,9 @@
 import secrets
 import os
 from PIL import Image
-from app import app
+from flask_mail import Message
+from flask import render_template
+from app import app, mail
 
 
 def save_picture_data(form_picture):
@@ -31,5 +33,18 @@ def remove_old_pic(filename):
         print(f"An error occurred: {e}!")
 
 
+def send_email(subject, recipients, text_body, html_body, sender="noreply@gymlog.me"):
+    msg = Message(subject, sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
+
+
 def send_password_reset_email(user):
-    pass
+    token = user.get_reset_password_token()
+    send_email(
+        "[GYMLOG]Reset Your Password",
+        [user.email],
+        render_template("email/reset_password.txt", user=user, token=token),
+        render_template("email/reset_password.html", user=user, token=token),
+    )
