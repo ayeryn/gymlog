9eaf1856691561b08d6a55de5ac1fa41a9d7ff3c
ayeryn <erynli@utexas.edu>
2023-12-21 15:31:57 -0600

    fix typo

--- app/models.py
+++ app/models.py
@@ -30,7 +30,7 @@ class User(db.Model, UserMixin):
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config["SECRET KEY"], algorithms=["HS256"])[
+            id = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])[
                 "reset_password"
             ]
         except:
