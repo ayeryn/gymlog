d99571531954d6adb0f015f44e95c15b707ebb66
ayeryn <erynli@utexas.edu>
2023-12-23 02:58:59 +0000

    remove debug statement

--- app/config.py
+++ app/config.py
@@ -7,7 +7,6 @@ class Config(object):
     SECRET_KEY = config.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    print(f"================DB IS {SQLALCHEMY_DATABASE_URI}============")
     ADMINS = ["erynli@utexas.edu"]
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
